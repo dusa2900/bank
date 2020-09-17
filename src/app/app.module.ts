3f579943a690ec0db10e6a3e38b2f47bb30cdfc8
dusa2900 <goutham2118185@gmail.com>
2020-09-17 10:17:52 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Body1Component } from './body1/body1.component';
+import { Body2Component } from './body2/body2.component';
+import { Body3Component } from './body3/body3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    Body1Component,
+    Body2Component,
+    Body3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
